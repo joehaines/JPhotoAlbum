077b2e8d188ba764e7225cdc7598478b88c90cf1
joehaines <joehaines@gmail.com>
2012-06-26 15:28:23 +0100

    First checkin of unchanged photo album code

---
+++ src/fi/iki/jka/JPhotoFrameTest.java
@@ -0,0 +1,25 @@
+package fi.iki.jka;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: localuser
+ * Date: 26/06/2012
+ * Time: 15:27
+ * To change this template use File | Settings | File Templates.
+ */
+public class JPhotoFrameTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testStartFullView() throws Exception {
+
+    }
+}
